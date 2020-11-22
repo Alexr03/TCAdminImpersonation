4390756a4644146c694b1f5794b70bb1e332d884
Alex Redding <Alexredding180@gmail.com>
2020-11-22 18:56:17 +0000

    Fix uninstall

--- uninstall.sql
+++ uninstall.sql
@@ -3,4 +3,4 @@
 DELETE FROM tc_permissions WHERE permission_id LIKE '1060';
 DELETE FROM tc_role_permissions WHERE permission_id LIKE '1060';
 
-DELETE FROM tc_page_icons WHERE page_id LIKE '1060';
+DELETE FROM tc_page_icons WHERE icon_id LIKE '1060';
